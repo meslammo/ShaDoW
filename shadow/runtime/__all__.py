d9e772ddcfe3cfb7d892bb7b09239d30baf1808e
mohamed meslam <meslammohamed423@gmail.com>
2026-09-10 07:12:30 +0300

    MOD-07.11 Expose runtime modules

---
+++ shadow/runtime/__all__.py
@@ -0,0 +1,4 @@
+from .runtime import RuntimeResult, ShadowRuntime
+from .capabilities import CAPABILITIES, requires_confirmation
+
+__all__ = ["RuntimeResult", "ShadowRuntime", "CAPABILITIES", "requires_confirmation"]
