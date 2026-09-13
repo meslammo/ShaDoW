ebd32d9a6930e282dbe95fc2deba12e4aef94ab1
mohamed meslam <meslammohamed423@gmail.com>
2026-09-13 04:41:32 +0300

    MOD-35.1: expose design package

---
+++ shadow/design/__init__.py
@@ -0,0 +1,4 @@
+"""SHADOW design helpers for site, building and CAD/BIM adapters."""
+from .site_building import DesignBrief, Rect, Site, propose, to_dict, to_dxf
+
+__all__ = ["DesignBrief", "Rect", "Site", "propose", "to_dict", "to_dxf"]
