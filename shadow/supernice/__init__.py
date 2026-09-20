f8cb1dff9fcaa8d5560bb8760234575a4b39ee6f
mohamed meslam <meslammohamed423@gmail.com>
2026-09-20 19:50:45 +0300

    MOD-159: expose complete Super Nice runtime surfaces

--- shadow/supernice/__init__.py
+++ shadow/supernice/__init__.py
@@ -2,5 +2,16 @@ from .catalog import CORE_BY_ID, CORE_SPECS
 from .runtime import CoreRuntime
 from .evolution import SelfEvolution
 from .integration import SuperNiceRuntime
+from .optional import OPTIONAL_DEVICE_CORE_IDS, device_integrations_enabled
+from .builtins import build_default_handlers
 
-__all__=["CORE_BY_ID","CORE_SPECS","CoreRuntime","SelfEvolution","SuperNiceRuntime"]
+__all__ = [
+    "CORE_BY_ID",
+    "CORE_SPECS",
+    "CoreRuntime",
+    "SelfEvolution",
+    "SuperNiceRuntime",
+    "OPTIONAL_DEVICE_CORE_IDS",
+    "device_integrations_enabled",
+    "build_default_handlers",
+]
