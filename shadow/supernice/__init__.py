24041d786b94c7140490411ce86f0cab03d13b9e
mohamed meslam <meslammohamed423@gmail.com>
2026-09-20 19:26:18 +0300

    SUPER-NICE-150.4: bridge 150-core layer to existing 12-core master runtime

--- shadow/supernice/__init__.py
+++ shadow/supernice/__init__.py
@@ -1,4 +1,6 @@
-from .catalog import CORE_BY_ID,CORE_SPECS
+from .catalog import CORE_BY_ID, CORE_SPECS
 from .runtime import CoreRuntime
 from .evolution import SelfEvolution
-__all__=["CORE_BY_ID","CORE_SPECS","CoreRuntime","SelfEvolution"]
+from .integration import SuperNiceRuntime
+
+__all__=["CORE_BY_ID","CORE_SPECS","CoreRuntime","SelfEvolution","SuperNiceRuntime"]
