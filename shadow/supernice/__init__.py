8facef7a32fe6419825033c77fe13bc7b6a94a61
mohamed meslam <meslammohamed423@gmail.com>
2026-09-25 12:48:03 +0300

    E2E-150.3: expose real online brain adapter

--- shadow/supernice/__init__.py
+++ shadow/supernice/__init__.py
@@ -4,6 +4,8 @@ from .evolution import SelfEvolution
 from .integration import SuperNiceRuntime
 from .optional import OPTIONAL_DEVICE_CORE_IDS, device_integrations_enabled
 from .builtins import build_default_handlers
+from .online_brain import RealOnlineBrainAdapter
+from .orchestrator import Unified150Orchestrator, UnifiedRunResult, OrchestratorEvent
 from .orchestrator import Unified150Orchestrator, UnifiedRunResult, OrchestratorEvent
 
 __all__ = [
@@ -15,6 +17,10 @@ __all__ = [
     "OPTIONAL_DEVICE_CORE_IDS",
     "device_integrations_enabled",
     "build_default_handlers",
+    "RealOnlineBrainAdapter",
+    "Unified150Orchestrator",
+    "UnifiedRunResult",
+    "OrchestratorEvent",
     "Unified150Orchestrator",
     "UnifiedRunResult",
     "OrchestratorEvent",
